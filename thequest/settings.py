8ba9b5e529b7c1411e49555dcc0c9a5719589fd3
Thienxlaoq <zpavlushchik@gmail.com>
2024-11-29 16:35:35 +0300

    s

--- thequest/settings.py
+++ thequest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-580hrnvq_8n#avbgsp!=)x3luf-)@t!5dovn2c5qx2%80=*uh%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
