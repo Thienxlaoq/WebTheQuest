0d8f18861bb1dcc378f3d7e1480b0136d2f3f4a0
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-01 05:00:10 +0300

    ы

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from ckeditor.fields import RichTextField
 
 class News(models.Model):
     title = RichTextField(help_text="Вы можете использовать HTML или стилизовать текст с разными цветами.")
-    description = models.TextField(blank=True, verbose_name='Текст новости:')
+    description = RichTextField(blank=True, verbose_name='Текст новости:')
     news_image = models.ImageField(upload_to='news-imgs', null=True, verbose_name='Изображение для новости')
     content_image = models.ImageField(upload_to='content_imgs', null=True, verbose_name='Фон для блока новости')
     published_date = models.DateField(auto_now_add=True, verbose_name='Дата публикации')
