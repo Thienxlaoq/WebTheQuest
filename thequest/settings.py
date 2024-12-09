fd0e1611029b0f4085154f7fcaf8439f34f5c609
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 05:09:54 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 # Статические файлы (CSS, JS, изображения для интерфейса)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
