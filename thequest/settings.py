e5d59645646f0e708a9c9896d8b0829a838ae616
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 16:46:58 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.thequest.pro','thequest.pro','thequest-72c3ecbb030c.herokuapp.com']
 
