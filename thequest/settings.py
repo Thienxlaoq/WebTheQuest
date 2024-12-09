5b0b926052fc3893de4d336461969d2c891a0e71
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 05:04:18 +0300

    s

--- thequest/settings.py
+++ thequest/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'ckeditor',
-    'whitenoise.runserver_nostatic', 
+    'whitenoise.runserver_nostatic',
     'storages',
 ]
 
