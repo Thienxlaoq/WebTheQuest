971ac4b8740da1936a216bcdc2794f2bec69526f
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 05:13:13 +0300

    sss

--- staticfiles/js/viewport.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const isInfoCenterPage = document.querySelector('.info-center-page') !== null;
-    if (isInfoCenterPage) {
-        const viewportMeta = document.createElement('meta');
-        viewportMeta.name = 'viewport';
-        viewportMeta.content = 'width=1024, initial-scale=0.3, maximum-scale=1, user-scalable=yes';
-        document.head.appendChild(viewportMeta);
-    }
-});
