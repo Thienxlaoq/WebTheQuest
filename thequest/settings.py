0311f7e8128190a48e48c122b3b00d0313866807
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 03:58:53 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -147,11 +147,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Медиа файлы (файлы, загружаемые пользователями)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
