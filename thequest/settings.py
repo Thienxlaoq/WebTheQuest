7ef1382062ebb61e4a5c52237c5b56b4a371a011
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 23:05:27 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import logging
-from google.oauth2 import service_account
-import json
-from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,18 +147,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GOOGLE_CLOUD_KEY_JSON = config('GOOGLE_CLOUD_KEY_JSON')
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True  # Перенаправление HTTP -> HTTPS
 
-
-GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
-    json.loads(GOOGLE_CLOUD_KEY_JSON)
-)
+# GCS настройки для медиа
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'thequest_website_bucket'
+GS_CREDENTIALS = '/app/key.json'
 MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/media/'
+
 
 WHITENOISE_AUTOREFRESH = True
 WHITENOISE_USE_FINDERS = True
