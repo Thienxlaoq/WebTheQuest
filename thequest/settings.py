73daeaa5b886ddfb3523b5a960b92b072dc17a12
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-09 22:05:28 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -154,12 +154,13 @@ GOOGLE_CLOUD_KEY_JSON = config('GOOGLE_CLOUD_KEY_JSON')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True  # Перенаправление HTTP -> HTTPS
+
+
 GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
     json.loads(GOOGLE_CLOUD_KEY_JSON)
 )
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'thequest_website_bucket'
-
 MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/media/'
 
 
