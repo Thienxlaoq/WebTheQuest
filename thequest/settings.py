b1ee5839f925ff1cfb5c78f9fa6fb2b4fb89d30a
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-10 11:35:11 +0300

    s

--- thequest/settings.py
+++ thequest/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'thequest.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
     )
 }
 
