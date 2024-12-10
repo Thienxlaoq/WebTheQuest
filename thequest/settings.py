28e3e1b15d6ad5285c34d0efaec3a796b1c92257
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-10 09:43:04 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
 import logging
 import json
+import base64
 from google.oauth2 import service_account
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
