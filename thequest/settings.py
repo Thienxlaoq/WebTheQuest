35658c729452916cc05efdaa3a6c7d0ed0a26756
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-10 15:08:55 +0300

    Update settings.py

--- thequest/settings.py
+++ thequest/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import logging
 import base64
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,10 +91,9 @@ WSGI_APPLICATION = 'thequest.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
