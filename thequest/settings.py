7007bb742b63ccebdfd025eb9acf34440c205d15
Thienxlaoq <zpavlushchik@gmail.com>
2024-12-12 11:42:37 +0300

    settings

--- thequest/settings.py
+++ thequest/settings.py
@@ -12,27 +12,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-580hrnvq_8n#avbgsp!=)x3luf-)@t!5dovn2c5qx2%80=*uh%')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'False'
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='небезопасный_секрет_для_локали')
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'www.thequest.pro', 'thequest.pro', 'thequest-72c3ecbb030c.herokuapp.com', 
+    'www.thequest.pro', 'thequest.pro',
+    'thequest-72c3ecbb030c.herokuapp.com', 
     'crystalline-kangaroo-idxmc57hcytlvuga79qbm5y9.herokudns.com',
     'evening-cardinal-lu0kx4iwnnjoym754j49soy3.herokudns.com',
     'localhost', '127.0.0.1'
 ]
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,22 +40,19 @@ INSTALLED_APPS = [
     'storages',
 ]
 
-# Google Cloud Storage Configuration
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'thequestweb'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
-
-WHITENOISE_USE_FINDERS = True
 
 ROOT_URLCONF = 'thequest.urls'
 
@@ -83,10 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'thequest.wsgi.application'
 
-# Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+        default=f'sqlite:///{os.path.join(Path(__file__).resolve().parent.parent, "db.sqlite3")}',
+        conn_max_age=600,
+        ssl_require=False
     )
 }
 
@@ -106,7 +98,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
-
-WHITENOISE_AUTOREFRESH = True
